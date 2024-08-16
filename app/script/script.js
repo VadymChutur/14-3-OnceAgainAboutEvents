d0666ee76519d78d6c03759172c71e2b1d82b4a4
Vadym Chutur <falloutgreck@gmail.com>
2024-08-16 13:00:31 +0300

    upd

--- app/script/script.js
+++ app/script/script.js
@@ -11,3 +11,23 @@ function sum(a, b = 1) {
 }
 
 console.log(sum(7));
+
+const oneEl = document.querySelector('#bonus-one');
+console.log(oneEl);
+
+function myClick() {
+  console.log('click');
+  const twoEl = document.querySelector('#bonus-two');
+  twoEl.addEventListener('click', myClick2);
+}
+
+function myClick2() {
+  console.log(2222);
+  const twoEl = document.querySelector('#bonus-two');
+}
+
+function myClick3() {
+  console.log(3333);
+}
+
+oneEl.addEventListener('click', myClick, { once: true });
